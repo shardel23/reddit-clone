1d2ee175be09a7fbc9630dca0a651e7670f684eb
Shahar Dotan <shardel23@fb.com>
2021-02-23 17:47:16 +0200

    Add basic registration page

---
+++ client/src/components/InputField.tsx
@@ -0,0 +1,26 @@
+import { Input } from "@chakra-ui/input";
+import { FormControl, FormLabel, FormErrorMessage } from "@chakra-ui/react";
+import { useField } from "formik";
+import React, { InputHTMLAttributes } from "react";
+
+type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
+  label: string;
+  name: string;
+  placeholder?: string;
+};
+
+export const InputField: React.FC<InputFieldProps> = (_size, props) => {
+  const [field, { error }] = useField(props);
+  return (
+    <FormControl isInvalid={!!error}>
+      <FormLabel htmlFor={field.name}>{props.label}</FormLabel>
+      <Input
+        {...field}
+        {...props}
+        id={field.name}
+        placeholder={props.placeholder}
+      />
+      {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
+    </FormControl>
+  );
+};
---
+++ client/src/pages/register.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Form, Formik } from "formik";
+import { Input } from "@chakra-ui/input";
+import { Button, FormControl, FormLabel } from "@chakra-ui/react";
+import { Wrapper } from "../components/Wrapper";
+import { InputField } from "../components/InputField";
+
+interface registerProps {}
+
+export const Register: React.FC<registerProps> = ({}) => {
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="username"
+              label="Username"
+              placeholder="username"
+            />
+            <InputField
+              name="password"
+              label="Password"
+              placeholder="password"
+              type="password"
+            />
+            <Button
+              type="submit"
+              colorScheme="facebook"
+              isLoading={isSubmitting}
+            >
+              Register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default Register;
