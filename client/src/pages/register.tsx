3286f2899e56d60d592d2bab269ff3bd82a13f6a
Shahar Dotan <shardel23@fb.com>
2021-02-24 21:03:01 +0200

    [Web] Handle register graphql response + refactoring

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -1,36 +1,29 @@
 import React from "react";
 import { Form, Formik } from "formik";
-import { Button, FormControl, FormLabel } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
 import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputField";
-import { useMutation, useQuery } from "urql";
+import { useRegisterMutation } from "../generated/graphql";
+import { toErrorMap } from "../utils/toErrorMap";
+import { useRouter } from "next/router";
 
 interface registerProps {}
 
-const registerGraphQL = `
-  mutation Register($username: String!, $password: String!) {
-    register(options: { username: $username, password: $password }) {
-      user {
-        id
-        username
-      }
-      errors {
-        field
-        message
-      }
-    }
-  }
-`;
-
 export const Register: React.FC<registerProps> = ({}) => {
-  const [result, registerMutation] = useMutation(registerGraphQL);
+  const [, register] = useRegisterMutation();
+  const router = useRouter();
 
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={(values) => {
-          registerMutation(values);
+        onSubmit={async (values, { setErrors }) => {
+          const response = await register(values);
+          if (response.data?.register.errors) {
+            setErrors(toErrorMap(response.data.register.errors));
+          } else if (response.data?.register.user) {
+            router.push("/");
+          }
         }}
       >
         {({ isSubmitting }) => (
