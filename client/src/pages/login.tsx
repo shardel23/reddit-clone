8b49457d7fe8f8c5f1964635d42c46e75a4665fb
Shahar Dotan <shardel23@fb.com>
2021-02-25 08:23:52 +0200

    Add login page

---
+++ client/src/pages/login.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Form, Formik } from "formik";
+import { Button } from "@chakra-ui/react";
+import { Wrapper } from "../components/Wrapper";
+import { InputField } from "../components/InputField";
+import { useLoginMutation } from "../generated/graphql";
+import { toErrorMap } from "../utils/toErrorMap";
+import { useRouter } from "next/router";
+
+export const Login: React.FC<{}> = ({}) => {
+  const [, login] = useLoginMutation();
+  const router = useRouter();
+
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={async (values, { setErrors }) => {
+          const response = await login({ options: values });
+          if (response.data?.login.errors) {
+            setErrors(toErrorMap(response.data.login.errors));
+          } else if (response.data?.login.user) {
+            router.push("/");
+          }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="username"
+              label="Username"
+              placeholder="username"
+            />
+            <InputField
+              name="password"
+              label="Password"
+              placeholder="password"
+              type="password"
+            />
+            <Button
+              type="submit"
+              colorScheme="facebook"
+              isLoading={isSubmitting}
+            >
+              Login
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default Login;
