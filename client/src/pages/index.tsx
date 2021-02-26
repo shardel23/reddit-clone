a2ceced46cb2d68cfa7abcde69049f138a946bf5
Shahar Dotan <shardel23@fb.com>
2021-02-26 10:58:49 +0200

    Add posts graphql query

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -2,12 +2,22 @@ import React from "react";
 import { NavBar } from "../components/NavBar";
 import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../utils/createUrqlClient";
+import { usePostsQuery } from "../generated/graphql";
 
-const Index = () => (
-  <>
-    <NavBar />
-    <div> Hello World </div>
-  </>
-);
+const Index = () => {
+  const [{ data }] = usePostsQuery();
+  return (
+    <>
+      <NavBar />
+      <h1> Posts: </h1>
+      <br />
+      {!data ? (
+        <div> Loading... </div>
+      ) : (
+        data.posts.map((post) => <div key={post.id}> {post.title} </div>)
+      )}
+    </>
+  );
+};
 
 export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
