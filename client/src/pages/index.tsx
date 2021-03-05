76855836c634d486b0b7d2cf6fd4fbc74bf6ea2a
Shahar Dotan <shardel23@fb.com>
2021-03-05 16:52:55 +0200

    [C] Add link from home page to create post

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,14 +1,20 @@
 import React from "react";
-import { NavBar } from "../components/NavBar";
 import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import { usePostsQuery } from "../generated/graphql";
+import { Layout } from "../components/Layout";
+import { Link } from "@chakra-ui/react";
+import NextLink from "next/link";
 
 const Index = () => {
   const [{ data }] = usePostsQuery();
   return (
-    <>
-      <NavBar />
+    <Layout>
+      <NextLink href="create-post">
+        <Link> Create new post </Link>
+      </NextLink>
+      <br />
+      <br />
       <h1> Posts: </h1>
       <br />
       {!data ? (
@@ -16,7 +22,7 @@ const Index = () => {
       ) : (
         data.posts.map((post) => <div key={post.id}> {post.title} </div>)
       )}
-    </>
+    </Layout>
   );
 };
 
