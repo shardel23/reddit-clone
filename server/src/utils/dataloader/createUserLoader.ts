e6028f021ef9d0a21a6811690de4ab8c64ad65e5
Shahar Dotan <shardel23@fb.com>
2021-03-30 21:45:43 +0300

    Fixed issue of owner/post missmatch

--- server/src/utils/dataloader/createUserLoader.ts
+++ server/src/utils/dataloader/createUserLoader.ts
@@ -4,5 +4,11 @@ import { User } from "../../entities/User";
 export const createUserLoader = () =>
   new DataLoader<number, User>(async (userIds) => {
     const users = await User.findByIds(userIds as number[]);
-    return users;
+
+    const userIdToUser: Record<number, User> = {};
+    users.forEach((u) => {
+      userIdToUser[u.id] = u;
+    });
+
+    return userIds.map((userId) => userIdToUser[userId]);
   });
